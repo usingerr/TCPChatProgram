0188c90af383f8f95e5658956d745553a7f13068
nscalese <n.scalese@yahoo.com>
2018-10-31 11:30:02 -0400

    Merge

--- MsgServer.py
+++ MsgServer.py
@@ -10,15 +10,20 @@ import json;
 
 chatrooms = [];
 myAddress = ('', 3333);
+defaultCnt = 1
 
 def getChatroom(addr):
     for chatroom in range(0, len(chatrooms)):
-        for client in chatroom[chatroom]:
+        for client in chatroom[chatroom]['room']:
             if client['addr'] == addr:
                 return chatroom;
             
-def leaveChatroom(addr):
-
+def leaveChatroom(addr, currChatroom):
+    room = chatrooms[currChatroom]['room']
+    for client in range(0, len(room)):
+        if room[client]['addr'] == addr:
+            room.pop(client);
+            break;
 def now():
     return time.ctime(time.time())
 
@@ -31,23 +36,30 @@ class MyClientHandler(socketserver.BaseRequestHandler):
             data = json.loads(data);
             
             if data['n']:
-                chatrooms.append({"addr": self.client_address, "req": self.request});
+                chatrooms.append({"name": "default" + str(defaultCnt), "room": [{"addr": self.client_address, "req": self.request, "nickname": self.client_address}]});
+                defaultCnt += 1;
             elif data['r']:
                 strChatrooms = 'Chatrooms:'
                 for index in range(0, len(chatrooms)):
-                    strChatrooms += (index + 1);
+                    strChatrooms += ' ' + chatrooms[index]['name'];
                 self.request.send(strChatrooms.decode());
             elif data['j']:
-                chatrooms[data['j'] - 1].append({"addr": self.client_address, "req": self.request});
+                chatrooms[data['j'] - 1].append({"addr": self.client_address, "req": self.request, "nickname": self.client_address});
             elif data['e']:
                 
                 break
             elif data['m']:
                 currChatroom = getChatroom(self.client_address);
-                for client in chatrooms[currChatroom]:
-                    if client.addr == self.client_address:
-                        continue
-                    client.request.send(data['m'].encode());
+                if data['m'] == '!leave':
+                    leaveChatroom(self.client_address, currChatroom);
+                    if not chatrooms[currChatroom]:
+                        chatrooms.pop(currChatroom);
+                    break;
+                else:
+                    for client in chatrooms[currChatroom]:
+                        if client.addr == self.client_address:
+                            continue
+                        client.request.send(data['m'].encode());
         
         self.request.close();
 
