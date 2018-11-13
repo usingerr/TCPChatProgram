942692e2d36c4869d39a4a1a0b8c6df99692a8dd
nscalese <n.scalese@yahoo.com>
2018-11-13 10:03:23 -0500

    Changes to command msgs on server side and client msg handling

--- MsgServer.py
+++ MsgServer.py
@@ -8,19 +8,8 @@ Created on Tue Oct 30 21:54:43 2018
 import socketserver, time;
 
 chatrooms = [{'name': 'default', 'room': []}];
+cmdList = [{'name': '!list', 'desc': ''}, {'name': '!help', 'desc': ''},{'name': '!leave', 'desc': ''}, {'name': '!new', 'desc': ''}, {'name': '!join', 'desc': ''}, {'name': '!nick', 'desc': ''}]
 myAddress = ('', 3333);
-switch = cmdSwitcher();
-defaultCnt = 1
-
-def getChatroom(addr):
-    for chatroom in range(0, len(chatrooms)):
-        for client in chatroom[chatroom]['room']:
-            if client['addr'] == addr:
-                return chatroom;
-            
-
-def now():
-    return time.ctime(time.time())
 
 class MyClientHandler(socketserver.BaseRequestHandler):
     def handle(self):                         
@@ -31,52 +20,90 @@ class MyClientHandler(socketserver.BaseRequestHandler):
         
             if firstConnect:
                 self.request.send(helpChatroom().encode());
-                self.request.send(listChatroom().encode());                
+                self.request.send(listChatroom().encode());    
+                firstConnect = False;
             
             currChatroom = getChatroom(self.client_address);
+            currRoom = chatrooms[currChatroom]['room'];
+            currClient = getClient(self.client_address);
             if data.startsWith('!'):
                 if data == '!help':
                     self.request.send(helpChatroom().encode());
                 elif data == '!list':
-                    self.request.send(listChatroom());
+                    self.request.send(listChatroom().encode());
                 elif data == '!leave':
-                    leaveChatroom(self.client_address, currChatroom);
+                    poppedClient = leaveChatroom(currClient, currChatroom);
+                    for client in currRoom:
+                        client['req'].send(('User ' + poppedClient['nickname'] + ' has left the chatroom.').encode());
                     break;
                 elif data == '!new':
-                    chatrooms.append({"name": "default" + str(defaultCnt), "room": [{"addr": self.client_address, "req": self.request, "nickname": self.client_address}]});
-                    defaultCnt += 1;
+                    chatrooms.append({"name": data[5:], "room": [{"addr": self.client_address, "req": self.request, "nickname": self.client_address}]});
                 elif data == '!join':
-                    chatrooms[data['j'] - 1].append({"addr": self.client_address, "req": self.request, "nickname": self.client_address});
+                    if(checkRoomExists(data[6:])):
+                        chatrooms[data[6:]['room']].append({"addr": self.client_address, "req": self.request, "nickname": self.client_address});
+                    else:
+                        self.request.send(('The room "' + data[6:] + '" does not exist.').encode());
+                elif data == '!nick':
+                    currClient['nickname'] = data[6:];
+                else:
+                    self.request.send(('"' + data + '" is not a recognized command.').encode());
+                    
             else:
-                for client in chatrooms[currChatroom]['room']:
-                    if client['addr'] == self.client_address:
+                for client in currRoom:
+                    if client == currClient:
                         continue
-                    client.request.send(data.encode());
+                    client['req'].send(makeMessage(currClient['nickname'], data).encode());
         
         self.request.close();
 
 msgServer = socketserver.ThreadingTCPServer(myAddress, MyClientHandler);
 msgServer.serve_forever();
 
+def now():
+    return time.ctime(time.time());
+
+def getChatroom(addr):
+    for chatroom in range(0, len(chatrooms)):
+        for client in chatrooms[chatroom]['room']:
+            if client['addr'] == addr:
+                return chatroom;
+            
+def getClient(addr, currChatroom):
+    for client in currChatroom:
+        if client['addr'] == addr:
+            return client;
+
 def listChatroom():
     strChatrooms = 'Chatrooms:'
-    for index in range(0, len(chatrooms)):
-        strChatrooms += ' ' + chatrooms[index]['name'];
+    for chatroom in range(0, len(chatrooms)):
+        strChatrooms += '\n' + str(chatroom) + '. ' + chatrooms[chatroom]['name'];
         
     return strChatrooms;
         
-def leaveChatroom(addr, currChatroom):
-    room = chatrooms[currChatroom]['room']
-    for client in range(0, len(room)):
-        if room[client]['addr'] == addr:
-            room.pop(client);
+def leaveChatroom(currClient, currChatroom):
+    room = chatrooms[currChatroom]['room'];
+    poppedClient = '';
+    for client in room:
+        if client == currClient:
+            poppedClient = room.remove(currClient);
             break;
             
-    if not room:
+    if not currChatroom:
         chatrooms.pop(currChatroom);
+        
+    return poppedClient;
 
 def helpChatroom():
-    return '';
+    strCommands = 'The current commands are:';
+    for cmd in cmdList:
+        strCommands += '\n' + cmd['name'] + ': ' + cmd['desc'];
+    return strCommands;
+
+def checkRoomExists(name):
+    for chatroom in chatrooms:
+        if(chatroom['name'] == name):
+            return True;
+    return False
 
 
 def makeMessage(nickName, message):
